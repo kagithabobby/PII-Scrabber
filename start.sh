6fe8b278b765dc93cbd8eee21136372c0afe872e
kagithabobby <bobbykagitha1980@gmail>
2026-04-18 08:34:31 +0530

    spacy for prodection update

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+# uvicorn main:app --host 0.0.0.0 --port 10000
+
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+
 uvicorn main:app --host 0.0.0.0 --port 10000
